fc99bf54097f2c71a13c3ff73f99970fe217d59e
Alexandr <zdesev-aleksandr@mail.ru>
2022-09-09 02:05:18 +0400

    add adopt refactoring

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function backToTop() {
   let button = $(".back-to-top");
 
   $(window).on("scroll", () => {
-    if ($(this).scrollTop() >= 200) {
+    if ($(this).scrollTop() >= 400) {
       button.fadeIn();
     } else {
       button.fadeOut();

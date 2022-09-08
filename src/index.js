ec6af7ca0933fb7dcc414483d244cd52239ccf6b
Alexandr <zdesev-aleksandr@mail.ru>
2022-09-09 02:43:40 +0400

    add some refactoring acordions and grid layout

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 // import './assets/css/main.css' //тестирование webpack
 // import mainImg from './assets/img/base_copy_8_50454 (1).png';
 
-document.querySelectorAll(".accordion-item__trigger").forEach((item) => {
-  item.addEventListener("click", () => {
-    const parent = item.parentNode;
-
-    if (parent.classList.contains("accordion-item--active")) {
-      parent.classList.remove("accordion-item--active");
-    } else {
-      document
-        .querySelectorAll(".accordion-item")
-        .forEach((child) => child.classList.remove("accordion-item--active"));
-      parent.classList.add("accordion-item--active");
-    }
-  });
+$(document).ready(function () {
+    $('.accordion-item__trigger').click(function () {
+        $(this).next('.accordion-item__content').slideToggle();
+    })
 });
+
 
 function backToTop() {
   let button = $(".back-to-top");

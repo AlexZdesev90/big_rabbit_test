8ee569d38e336de0f586c4a6643352593540a881
Alexandr <zdesev-aleksandr@mail.ru>
2022-09-08 22:41:16 +0400

    add logic scroll back to top

--- src/index.js
+++ src/index.js
@@ -1,54 +1,35 @@
-// import './assets/css/main.css'
+// import './assets/css/main.css' //тестирование webpack
 // import mainImg from './assets/img/base_copy_8_50454 (1).png';
 
+document.querySelectorAll(".accordion-item__trigger").forEach((item) => {
+  item.addEventListener("click", () => {
+    const parent = item.parentNode;
 
-//js
-document.querySelectorAll('.accordion-item__trigger').forEach((item) => {
-    item.addEventListener('click', () => {
-        const parent = item.parentNode;
+    if (parent.classList.contains("accordion-item--active")) {
+      parent.classList.remove("accordion-item--active");
+    } else {
+      document
+        .querySelectorAll(".accordion-item")
+        .forEach((child) => child.classList.remove("accordion-item--active"));
+      parent.classList.add("accordion-item--active");
+    }
+  });
+});
 
-        if (parent.classList.contains('accordion-item--active')) {
-            parent.classList.remove('accordion-item--active');
-        } else {
-            document.querySelectorAll('.accordion-item').forEach((child) => child.classList.remove('accordion-item--active'))
+function backToTop() {
+  let button = $(".back-to-top");
 
-            parent.classList.add('accordion-item--active')
-        }
-
-        
-    })
-})
-
-//jquery
-
-// $(document).ready(function () {
-//     $('.accordion-item__trigger').click(function () {
-//         const parent = $(this).parent()
-
-//         if (parent.hasClass('accordion-item--active')) {
-//             parent.removeClass('accordion-item--active');
-//         } else {
-//             $('.accordion-item').removeClass('accordion-item--active')
-//             parent.addClass('accordion-item--active')
-//         }
-//     });
-// });
-
-function backToTop(){
-    let button = $('.back-to-top');
-
-    $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 200) {
-            button.fadeIn();
-        } else {
-            button.fadeOut();
-        }
-    });
-    button.on('click', (e) => {
-        e.preventDefault();
-        $('html').animate({scrollTop: 0}, 1000)
-    })
+  $(window).on("scroll", () => {
+    if ($(this).scrollTop() >= 200) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+  button.on("click", (e) => {
+    e.preventDefault();
+    $("html").animate({ scrollTop: 500 }, 1000);
+  });
 }
 
-backToTop()
-
+backToTop();
